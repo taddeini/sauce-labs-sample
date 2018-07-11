7ead9ab6a3ebf9058e19811527e707c79dd546d8
Andre Taddeini <andre.taddeini@bestbuy.com>
2018-07-11 11:18:39 -0500

    command timeout testing

--- src/examples/mocha-timeout.js
+++ src/examples/mocha-timeout.js
@@ -3,7 +3,10 @@ const assert = require('chai').assert;
 describe('[Sauce Labs Sample] - demo', () => {
     it('should fail due to timeouts', () => {
         // 1) Set mochaOpts.timeout value to 1000 
+        //browser.url('https://www.bestbuy.com.mx/');
+
         // 2) Set commandTimeout value to 1
         browser.url('https://www.bestbuy.com.mx/');
+        browser.waitForExist('#does-not-exist');
     });
 });
