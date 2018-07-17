268540abcdcafe7cb7ac8139f4f7ce2e1d8a6e40
Andre Taddeini <andre.taddeini@bestbuy.com>
2018-07-17 14:11:43 -0500

    starting point

--- src/examples/demo.js
+++ src/examples/demo.js
@@ -9,14 +9,5 @@ describe('[Sauce Labs Sample] - demo', () => {
         // Search
         browser.setValue('#gh-search-input', 'apple');
         browser.element('.hf-icon-search').click();
-
-        // TIMEOUTS
-        // 1) Set mochaOpts.timeout value    
-        // 2) Set commandTimeout
-        // browser.execute(function () {
-        //     while (true) {
-        //         console.log('Uh oh...');
-        //     }
-        // });
     });
 });
